c21a9f657b2ffb057f4b24f91235f1322c4b0dc0
anjanak05 <anjna4523@gmail.com>
2022-08-24 20:34:04 +0530

    start

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
